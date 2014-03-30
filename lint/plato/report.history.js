8e39fadbd1c36043da3d7e20f8065925b0cdf87a
obara.takao <obara.takao@dena.jp>
2014-03-31 02:06:51 +0900

    add EventListener

--- lint/plato/report.history.js
+++ lint/plato/report.history.js
@@ -9,5 +9,16 @@ __history = [
       "sloc": 336,
       "maintainability": "66.67"
     }
+  },
+  {
+    "date": "Sun, 30 Mar 2014 17:05:37 GMT",
+    "total": {
+      "sloc": 375,
+      "maintainability": 67.61176426579532
+    },
+    "average": {
+      "sloc": 375,
+      "maintainability": "67.61"
+    }
   }
 ]
--- test/index.node.js
+++ test/index.node.js
@@ -2,6 +2,7 @@ require("uupaa.valid.js");
 require("uupaa.task.js");
 require("uupaa.test.js");
 require("uupaa.uri.js");
+require("uupaa.eventlistener.js");
 require("uupaa.bytearray.js");
 require("uupaa.nodeproxy.js");
 require("../lib/Proxy.js");
