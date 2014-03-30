ce6311df9098e5df8b7b3f09d154340be9274d5e
obara.takao <obara.takao@dena.jp>
2014-03-30 20:33:07 +0900

    first revision

---
+++ lint/plato/report.history.js
@@ -0,0 +1,13 @@
+__history = [
+  {
+    "date": "Sun, 30 Mar 2014 11:31:59 GMT",
+    "total": {
+      "sloc": 336,
+      "maintainability": 66.66665691836717
+    },
+    "average": {
+      "sloc": 336,
+      "maintainability": "66.67"
+    }
+  }
+]
---
+++ test/index.node.js
@@ -0,0 +1,10 @@
+require("uupaa.valid.js");
+require("uupaa.task.js");
+require("uupaa.test.js");
+require("uupaa.uri.js");
+require("uupaa.bytearray.js");
+require("uupaa.nodeproxy.js");
+require("../lib/Proxy.js");
+require("../lib/Proxy.min.js");
+require("./test.js");
+
